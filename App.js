71be6ec2912b99df7bc2875a35bc93c5c12df86c
TylerKang-97 <kingksy777@gmail.com>
2021-11-05 18:04:32 +0900

    InputText Test_1105

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './src/routes/Home';
 import Details from './src/routes/Details';
+import Test from './src/routes/Test';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,11 +14,10 @@ function App() {
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={Home} navigation={Stack.Navigator} />
                 <Stack.Screen name="Details" component={Details} navigation={Stack.Navigator} />
+                <Stack.Screen name="Test" component={Test} navigation={Stack.Navigator} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
 
 export default App;
-
-//협업 되나?
---
+++ src/routes/Test.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+const Test = () => {
+    const [comment, setComment ] = useState("");
+
+    return (
+      <View style={styles.container}>
+        <Text>
+              {comment ? comment : "글을 입력해 주세요 :)" }
+          </Text>
+        <Text>Welcome Tyler!</Text>
+        <StatusBar style="auto" />
+        <TextInput 
+          style={styles.input}
+          onChangeText={text => setComment(text)}
+          value={comment}
+          returnKeyType="done"
+          />
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FDEAEB',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+export default Test;
